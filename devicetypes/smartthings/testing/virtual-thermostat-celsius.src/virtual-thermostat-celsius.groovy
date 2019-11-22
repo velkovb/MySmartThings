5d5741d4911359b773a69bf9d7d72bde0c7555d0
Borislav Velkov <velkov.borislav@gmail.com>
2019-11-22 19:00:33 +0200

    remove am/pm

--- devicetypes/smartthings/testing/virtual-thermostat-celsius.src/virtual-thermostat-celsius.groovy
+++ devicetypes/smartthings/testing/virtual-thermostat-celsius.src/virtual-thermostat-celsius.groovy
@@ -333,8 +333,8 @@ private initialize() {
     sendEvent(name: "coolingSetpointMin", value: COOLING_SETPOINT_RANGE.getFrom(), unit: "°C")
     sendEvent(name: "coolingSetpointMax", value: COOLING_SETPOINT_RANGE.getTo(), unit: "°C")
     sendEvent(name: "thermostatMode", value: DEFAULT_MODE)
-    sendEvent(name: "schedule", value: Date.parse("yyyy-MM-dd'T'HH:mm:ss.SSS",dayTime).format("HH:mm"))
-    sendEvent(name: "speed", value: Date.parse("yyyy-MM-dd'T'HH:mm:ss.SSS",nightTime).format("HH:mm"))
+    sendEvent(name: "schedule", value: Date.parse("yyyy-MM-dd'T'HH:mm:ss.SSS",dayTime).format("H:mm"))
+    sendEvent(name: "speed", value: Date.parse("yyyy-MM-dd'T'HH:mm:ss.SSS",nightTime).format("H:mm"))
     //sendEvent(name: "thermostatFanMode", value: DEFAULT_FAN_MODE)
     sendEvent(name: "thermostatOperatingState", value: DEFAULT_OP_STATE)
 
@@ -372,8 +372,8 @@ def refresh() {
     sendEvent(name: "heatingSetpoint", value: getHeatingSetpoint(), unit: "°C")
     sendEvent(name: "temperature", value: getTemperature(), unit: "°C")
     sendEvent(name: "humidity", value: getHumidityPercent(), unit: "%")
-    sendEvent(name: "schedule", value: Date.parse("yyyy-MM-dd'T'HH:mm:ss.SSS",dayTime).format("H:mm a"))
-    sendEvent(name: "speed", value: Date.parse("yyyy-MM-dd'T'HH:mm:ss.SSS",nightTime).format("H:mm a"))
+    sendEvent(name: "schedule", value: Date.parse("yyyy-MM-dd'T'HH:mm:ss.SSS",dayTime).format("H:mm"))
+    sendEvent(name: "speed", value: Date.parse("yyyy-MM-dd'T'HH:mm:ss.SSS",nightTime).format("H:mm"))
     done()
 }
 
